7cc338170def4444c4e3d9a583efdd3ec529f5b5
Rich Wareham <rjw57@cam.ac.uk>
2015-10-23 12:40:01 +0100

    add docutils as a requirement
    Issue #4 notes that docutils is a requirement. Reflect this in the ``setup.py``
    script. For the moment leave the same information in ``requirements.txt``. The
    difference between the two approaches is subtle and, unless one is sure, it is
    probably worth the hit of being a little wet (i.e. non-DRY).

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ setup(
     name='mimic',
     version="0.0.1",
     packages=find_packages(),
+    install_requires=['docutils'],
     entry_points={
         'console_scripts': [
             'mimic=mimic:main',
